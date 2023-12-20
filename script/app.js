b08b8cf59e90d71fe8c44a7856ad482869213865
Виктория <122749500+Viktoria1291@users.noreply.github.com>
2023-12-20 18:08:53 +0300

    scroll

--- script/app.js
+++ script/app.js
@@ -1,3 +1,4 @@
+// Это для параллакса:
 window.addEventListener('scroll', e => {
 	document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`);
 })
@@ -9,9 +10,35 @@ ScrollSmoother.create({
 	content: '.content'
 });
 
-/*ScrollTrigger.create({ 
-	trigger: ".secret1", 
-	start: "top top", 
-	end: "bottom bottom", 
-	pin: true, 
-	markers: true });*/
+
+
+// Это для двух видов скролла (вниз и вглубь):
+window.addEventListener("scroll", function () {
+	var pic2 = document.getElementById("secret1");
+	var pic3 = document.getElementById("secret2");
+	var pic4 = document.getElementById("secret3");
+
+	var scrollPosition = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+	if (scrollPosition >= pic2.offsetTop && scrollPosition < pic2.offsetTop + pic2.offsetHeight) {
+		pic2.style.position = "fixed";
+		pic2.style.top = "0";
+	} else {
+		console.log('Ошибка');
+		pic2.style.position = "relative";
+	}
+/*
+	if (scrollPosition >= pic3.offsetTop && scrollPosition < pic3.offsetTop + pic3.offsetHeight) {
+		pic3.style.position = "fixed";
+		pic3.style.top = "0";
+	} else {
+		pic3.style.position = "relative";
+	}
+
+	if (scrollPosition >= pic4.offsetTop && scrollPosition < pic4.offsetTop + pic4.offsetHeight) {
+		pic4.style.position = "fixed";
+		pic4.style.top = "0";
+	} else {
+		pic4.style.position = "relative";
+	}*/
+});
