6b5272103f6735252138a8083d1d85fd5b508f07
Виктория <122749500+Viktoria1291@users.noreply.github.com>
2024-01-18 11:57:01 +0300

    first commit

---
+++ Readme.txt
@@ -0,0 +1,2 @@
+Сайт с Parallax effect. Написан на HTML, CSS, JavaScript.
+Проведена работа с анимацией (Cubic Bezier), плавностью
--- script/app.js
+++ script/app.js
@@ -9,36 +9,3 @@ ScrollSmoother.create({
 	wrapper: '.wrapper',
 	content: '.content'
 });
-
-
-
-// Это для двух видов скролла (вниз и вглубь):
-window.addEventListener("scroll", function () {
-	var pic2 = document.getElementById("secret1");
-	var pic3 = document.getElementById("secret2");
-	var pic4 = document.getElementById("secret3");
-
-	var scrollPosition = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0;
-
-	if (scrollPosition >= pic2.offsetTop && scrollPosition < pic2.offsetTop + pic2.offsetHeight) {
-		pic2.style.position = "fixed";
-		pic2.style.top = "0";
-	} else {
-		console.log('Ошибка');
-		pic2.style.position = "relative";
-	}
-/*
-	if (scrollPosition >= pic3.offsetTop && scrollPosition < pic3.offsetTop + pic3.offsetHeight) {
-		pic3.style.position = "fixed";
-		pic3.style.top = "0";
-	} else {
-		pic3.style.position = "relative";
-	}
-
-	if (scrollPosition >= pic4.offsetTop && scrollPosition < pic4.offsetTop + pic4.offsetHeight) {
-		pic4.style.position = "fixed";
-		pic4.style.top = "0";
-	} else {
-		pic4.style.position = "relative";
-	}*/
-});
